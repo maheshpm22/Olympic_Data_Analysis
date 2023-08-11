e5934b1e642bda1261c216c9aa3e93a7b7f7c02c
maheshpm22 <140488099+maheshpm22@users.noreply.github.com>
2023-08-11 23:14:48 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -43,10 +43,11 @@ def country_year_list(df):
 
 def data_over_time(df,col):
 
-    nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index().sort_values('index')
+    # nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index().sort_values('index')
     # nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index()
     # nations_over_time.rename(columns={'index':'Edition', 'Year': col}, inplace=True)
-    nations_over_time.rename(columns={'index': 'Edition', 'Year': col}, inplace=True)
+    nations_over_time = df.drop_duplicates(['Year', 'region' ])['Year'].value_counts().reset_index().sort_values('Year')
+    nations_over_time.rename(columns = { 'Year' : 'Edition' , 'count' : 'No of Countries' }, inplace = True ) 
     # nations_over_time = nations_over_time.sort_values('Edition') 
     return nations_over_time
 
