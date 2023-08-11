bb9e3ac0ae3a53798709339c4b57c6409d19fd5e
maheshpm22 <140488099+maheshpm22@users.noreply.github.com>
2023-08-12 00:05:37 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -41,16 +41,17 @@ def country_year_list(df):
 
     return years,country
 
+# def data_over_time(df,col):
+#     nations_over_time = df.drop_duplicates(['Year', 'region' ])['Year'].value_counts().reset_index().sort_values('Year')
+#     nations_over_time.rename(columns = { 'Year' : 'Edition' , 'count' : 'No of Countries' }, inplace = True ) 
+#     return nations_over_time
+
 def data_over_time(df,col):
 
-    # nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index().sort_values('index')
-    # nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index()
-    # nations_over_time.rename(columns={'index':'Edition', 'Year': col}, inplace=True)
-    nations_over_time = df.drop_duplicates(['Year', 'region' ])['Year'].value_counts().reset_index().sort_values('Year')
-    nations_over_time.rename(columns = { 'Year' : 'Edition' , 'count' : 'No of Countries' }, inplace = True ) 
-    # nations_over_time = nations_over_time.sort_values('Edition') 
+    nations_over_time = df.drop_duplicates(['Year', col ])['Year'].value_counts().reset_index().sort_values('Year')
+    nations_over_time.rename(columns = { 'Year' : 'Edition' , 'count' : col }, inplace = True ) 
     return nations_over_time
-
+    
 
 def most_successful(df, sport):
     temp_df = df.dropna(subset=['Medal'])
